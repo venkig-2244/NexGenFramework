4a988461f6bd372513f2bc1e40257a230b20a248
Venkatesh G <itsvenkatesh@gmail.com>
2022-07-03 16:22:07 +0530

    added python test

---
+++ NexGenTest/NexGenTest/NexGenTest.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+
+def main():
+    print("NexGen Test starting..")
+
+    driverService = Service('C:\\ChromeDriver\\chromedriver.exe')
+    driver = webdriver.Chrome(service=driverService)
+    driver.get("http://www.google.com")
+    
+    elem = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    
+    print("Waiting... ")
+    sleep(60)
+    print("Before Quit function")
+    driver.quit()
+    
+if __name__ == "__main__":
+    main()
+
--- TestScript/src/main/java/TestSurvApp.java
+++ TestScript/src/main/java/TestSurvApp.java
@@ -1,20 +1,24 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestSurvApp {
     public static void main(String[] args) throws InterruptedException {
 
         // System Property for Chrome Driver
-        System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\VenkyG\\ChromeDriver\\chromedriver.exe");
+
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("enable-precise-memory-info");
 
         // Instantiate a ChromeDriver class.
         WebDriver driver = new ChromeDriver();
 
         // Launch Website
-        driver.navigate().to("http://www.javatpoint.com/");
+        //driver.navigate().to("http://www.javatpoint.com/");
+        driver.get("http://www.javatpoint.com/");
 
         //Maximize the browser
         driver.manage().window().maximize();
@@ -25,5 +29,7 @@ public class TestSurvApp {
         Thread.sleep(5000);
 
         driver.quit();
+        System.out.println("Hello World!");
+//        Thread.sleep(5000);
     }
 }
