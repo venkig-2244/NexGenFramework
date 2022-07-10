6fd25df78ca281d1934609cca22656f7c877d7a6
Venkatesh G <itsvenkatesh@gmail.com>
2022-07-10 16:57:49 +0530

    Added logging code

--- NexGenTest/NexGenTest/NexGenTest.py
+++ NexGenTest/NexGenTest/NexGenTest.py
@@ -5,13 +5,19 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from logging.handlers import TimedRotatingFileHandler
+import logging as logger
 
 def main():
-    print("NexGen Test starting..")
+    logfile_name = 'nexgen.log'
+    logger.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logger.INFO)
+    
+    handler = TimedRotatingFileHandler("nxtgen.log",
+                                       when="d",
+                                       interval=1,
+                                       backupCount=100)
 
-  #  driverService = Service(ChromeDriverManager().install())
-    #driver = webdriver.Chrome(executable_path='../resources/chromedriver')
-
+    logger.getLogger().addHandler(handler)
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -20,22 +26,23 @@ def main():
     chrome_options.experimental_options["prefs"] = chrome_prefs
     chrome_prefs["profile.default_content_settings"] = {"images": 2}
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    for i in range(0, 5):
+        logger.info("NexGen Test starting..")
+        logger.info("Initializing Chromer driver..")
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    driver.get("http://www.google.com")
+        logger.info("Opening URL google.com")
+        driver.get("http://www.google.com")
     
-    elem = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
-    elem.send_keys("pycon")
-    elem.send_keys(Keys.RETURN)
+        elem = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
     
-    file = open("logs.txt")
-    file.write("Hello world")
-    file.close()
+        logger.info("Waiting... ")
+        sleep(60)
+        logger.warning("Test completed. Closing driver.")
 
-    print("Waiting... ")
-    sleep(60)
-    print("Before Quit function")
-    driver.quit()
+        driver.quit()
     
 if __name__ == "__main__":
     main()
