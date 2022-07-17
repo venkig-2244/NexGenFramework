3d0b38346f8f264d380d8c8c2c72d298d93216e1
Venkatesh G <itsvenkatesh@gmail.com>
2022-07-17 06:56:45 +0530

    Inclusion of InfluxDB

--- NexGenTest/NexGenTest/NexGenTest.py
+++ NexGenTest/NexGenTest/NexGenTest.py
@@ -6,18 +6,26 @@ from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from logging.handlers import TimedRotatingFileHandler
-import logging as logger
+import logging
+import ecs_logging
+from influxdb import InfluxDBClient
+from datetime import datetime
+import random
 
 def main():
-    logfile_name = 'nexgen.log'
-    logger.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logger.INFO)
+    logfile_name = 'nexgen.json'
+    #logger.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logger.INFO)
     
-    handler = TimedRotatingFileHandler(logfile_name,
-                                       when="d",
-                                       interval=1,
-                                       backupCount=100)
-
-    logger.getLogger().addHandler(handler)
+    #handler = TimedRotatingFileHandler(logfile_name,
+    #                                   when="d",
+    #                                   interval=1,
+    #                                   backupCount=100)
+    logger = logging.getLogger("nexgen")
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(logfile_name)
+    handler.setFormatter(ecs_logging.StdlibFormatter())
+    logger.addHandler(handler)
+    
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -26,23 +34,57 @@ def main():
     chrome_options.experimental_options["prefs"] = chrome_prefs
     chrome_prefs["profile.default_content_settings"] = {"images": 2}
 
-    while(True):
-        logger.info("NexGen Test starting..")
-        logger.info("Initializing Chromer driver..")
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    client = InfluxDBClient('20.219.137.165', 8086, 'admin', 'Password1', 'mydb')
+    #print(client.create_database('mydb'))
+    print(client.get_list_database())
+    print(client.switch_database('mydb'))
 
-        logger.info("Opening URL google.com")
-        driver.get("http://www.google.com")
-    
-        elem = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-    
-        logger.info("Waiting... ")
-        sleep(60)
-        logger.warning("Test completed. Closing driver.")
+    json_payload = []
+    data = {
+        "measurement": "stocks",
+        "tags": {
+            "ticker": "Infy" 
+            },
+        "time": datetime.now(),
+        "fields": {
+            'open': float(random.randint(2600, 2670)),
+            'close': float(random.randint(2600, 2670))
+        }
+    }
+    json_payload.append(data)
 
-        driver.quit()
+
+    #Send our payload
+    client.write_points(json_payload)
+
+    query = "select * from stocks;"
+    print("Querying data: " + query)
+    result = client.query(query)
+
+    for measurement in result.get_points(measurement='stocks'):
+        usage_system = measurement['ticker']
+        open = measurement['open']
+        close = measurement['close']
+        print("{0}\t{1}\t{2}".format(usage_system, open, close))
+
+    # while(1):
+    #     logger.info("NexGen Test starting..")
+    #     logger.info("Initializing Chromer driver..")
+    #     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+    #     logger.info("Opening URL google.com")
+    #     driver.get("http://www.google.com")
+    
+    #     elem = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+    #     elem.send_keys("pycon")
+    #     elem.send_keys(Keys.RETURN)
+    
+    #     logger.info("Waiting... ")
+    #     sleep(10)
+    #     logger.warning("Test completed. Closing driver.")
+
+    #     driver.quit()
+
     
 if __name__ == "__main__":
     main()
