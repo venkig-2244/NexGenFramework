57e3dc12de047f7d343cd293b6ba16ff08cf6528
Venkatesh G <itsvenkatesh@gmail.com>
2022-08-07 22:00:02 +0530

    NexGenEngine implementation

--- ExecutionEngine/NexGenEngine.py
+++ ExecutionEngine/NexGenEngine.py
@@ -1,11 +1,13 @@
+from typing import ParamSpec
 from kubernetes import client, config
 
-# Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
+class NexGenEngine(object):
+    def __init__(self):
+        config.load_kube_config()
+        self.v1 = client.CoreV1Api()
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    def printPods(self):    
+        ret = self.v1.list_pod_for_all_namespaces(watch=False)
+        for i in ret.items:
+            print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
     
