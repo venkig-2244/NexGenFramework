255bed0c6e19b3dc607f6a6e46ec6fa8a97da662
Venkatesh G <itsvenkatesh@gmail.com>
2022-08-07 21:50:40 +0530

    Updated code

--- NexGenTest/NexGenTest/NexGenTest.py
+++ NexGenTest/NexGenTest/NexGenTest.py
@@ -1,3 +1,4 @@
+from tokenize import Single
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
@@ -11,6 +12,165 @@ import ecs_logging
 from influxdb import InfluxDBClient
 from datetime import datetime
 import random
+
+class DBConnection(object):
+    
+    _connectionToken = "mySuP3rS3cr3tT0keN"
+    _dbConnection = "InfluxDB"
+
+    def __init__(self, ipAddress):
+        """virtual private constructor"""
+        if DBConnection._dbConnection != 'InfluxDB':
+            raise Exception("This class is a singleton class !")
+        else:
+            DBConnection._dbConnection = self
+
+        self.client = InfluxDBClient(ipAddress, 8086, username=None, password=None, headers={"Authorization": self._connectionToken})            
+        self.client.create_database('mydb')
+        print(self.client.get_list_database())
+        self.client.switch_database('mydb')        
+
+    @staticmethod
+    def getInstance():
+        """Static Access Method"""
+        if DBConnection._dbConnection == 'InfluxDB':
+            DBConnection()
+
+        return DBConnection._dbConnection
+
+
+    def writeDataToDB(self, jsonPayload):
+        # Send our payload
+        self.client.write_points(jsonPayload)
+
+class WriteResponseTimeToDB(object):
+    
+    dbConnection = DBConnection('20.219.137.165')
+    def __init__(self):
+        pass    
+
+    def WriteResponseTimeToDB(self, json_payload):
+        DBConnection.getInstance().writeDataToDB(json_payload)
+
+class ResponseTimeForLogin(WriteResponseTimeToDB):
+    def __init__(self):
+        self.measurementName = "ResponseTime"
+        WriteResponseTimeToDB.__init__(self)
+
+    def WriteResponseTime(self, responseTime):
+        json_payload = []    
+        data = {
+            "measurement": self.measurementName,
+            "tags": {
+                "ticker": "LoginRT" 
+                },
+            "fields": {
+                'responsetime': responseTime
+            }
+        }
+
+        json_payload.append(data)
+        return super().WriteResponseTimeToDB(json_payload)
+
+class ResponseTimeSearch(WriteResponseTimeToDB):
+    def __init__(self):
+        self.measurementName = "ResponseTime"
+        WriteResponseTimeToDB.__init__(self)
+
+    def WriteResponseTime(self, responseTime):
+        json_payload = []    
+        data = {
+            "measurement": self.measurementName,
+            "tags": {
+                "ticker": "SearchRT" 
+                },
+            "fields": {
+                'responsetime': responseTime
+            }
+        }
+
+        json_payload.append(data)
+        return super().WriteResponseTimeToDB(json_payload)
+
+class TransactionsPerSecond(WriteResponseTimeToDB):
+    def __init__(self):
+        self.measurementName = "Transactions"
+        WriteResponseTimeToDB.__init__(self)
+
+    def WriteTransactionTime(self, txPerSecond):
+        json_payload = []    
+        data = {
+            "measurement": self.measurementName,
+            "tags": {
+                "ticker": "TxPerSecond" 
+                },
+            "fields": {
+                'txTime': txPerSecond
+            }
+        }
+
+        json_payload.append(data)
+        return super().WriteResponseTimeToDB(json_payload)
+
+class CpuUsage(WriteResponseTimeToDB):
+    def __init__(self):
+        self.measurementName = "ResourceUsage"
+        WriteResponseTimeToDB.__init__(self)
+
+    def WriteUsagePercentage(self, cpuUsage):
+        json_payload = []    
+        data = {
+            "measurement": self.measurementName,
+            "tags": {
+                "ticker": "CPU" 
+                },
+            "fields": {
+                'usage': cpuUsage
+            }
+        }
+
+        json_payload.append(data)
+        return super().WriteResponseTimeToDB(json_payload)
+
+class MemoryUsage(WriteResponseTimeToDB):
+    def __init__(self):
+        self.measurementName = "ResourceUsage"
+        WriteResponseTimeToDB.__init__(self)
+
+    def WriteUsagePercentage(self, memUsage):
+        json_payload = []    
+        data = {
+            "measurement": self.measurementName,
+            "tags": {
+                "ticker": "Memory" 
+                },
+            "fields": {
+                'usage': memUsage
+            }
+        }
+
+        json_payload.append(data)
+        return super().WriteResponseTimeToDB(json_payload)
+
+class DiskUsage(WriteResponseTimeToDB):
+    def __init__(self):
+        self.measurementName = "ResourceUsage"
+        WriteResponseTimeToDB.__init__(self)
+
+    def WriteUsagePercentage(self, diskUsage):
+        json_payload = []    
+        data = {
+            "measurement": self.measurementName,
+            "tags": {
+                "ticker": "Disk" 
+                },
+            "fields": {
+                'usage': diskUsage
+            }
+        }
+
+        json_payload.append(data)
+        return super().WriteResponseTimeToDB(json_payload)
 
 def main():
     logfile_name = 'nexgen.json'
@@ -26,46 +186,57 @@ def main():
     handler.setFormatter(ecs_logging.StdlibFormatter())
     logger.addHandler(handler)
     
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_prefs = {}
-    chrome_options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_prefs = {}
+    # chrome_options.experimental_options["prefs"] = chrome_prefs
+    # chrome_prefs["profile.default_content_settings"] = {"images": 2}
 
-    client = InfluxDBClient('20.219.137.165', 8086, 'admin', 'Password1', 'mydb')
-    #print(client.create_database('mydb'))
-    print(client.get_list_database())
-    print(client.switch_database('mydb'))
+    index = 0
+    loginResponse = ResponseTimeForLogin()
+    searchResponse = ResponseTimeSearch()
+    txPerSec = TransactionsPerSecond()
+    cpuUsage = CpuUsage()
+    diskUsage = DiskUsage()
+    memUsage = MemoryUsage()
 
-    json_payload = []
-    data = {
-        "measurement": "stocks",
-        "tags": {
-            "ticker": "Infy" 
-            },
-        "time": datetime.now(),
-        "fields": {
-            'open': float(random.randint(2600, 2670)),
-            'close': float(random.randint(2600, 2670))
-        }
-    }
-    json_payload.append(data)
+    
+    while(index <= 100):
+        # json_payload = []    
+        # data = {
+        #     "measurement": "ResponseTime",
+        #     # "tags": {
+        #     #     "ticker": "Response" 
+        #     #     },
+        #     "time": datetime.now(),
+        #     "fields": {
+        #         'rtime': float(random.randint(2600, 3000))
+        #     }
+        # }
 
+        # json_payload.append(data)
+        loginResponse.WriteResponseTime(random.randint(5000, 5300))
+        searchResponse.WriteResponseTime(random.randint(1000,1200))
+        txPerSec.WriteTransactionTime(random.randint(1000,2000))
+        cpuUsage.WriteUsagePercentage(random.randint(90,99))
+        memUsage.WriteUsagePercentage(random.randint(50,80))
+        diskUsage.WriteUsagePercentage(random.randint(20,50))
+
+        index += 1
 
     #Send our payload
-    client.write_points(json_payload)
+#    client.write_points(json_payload)
 
-    query = "select * from stocks;"
-    print("Querying data: " + query)
-    result = client.query(query)
+    # query = "select * from ResponseTime;"
+    # print("Querying data: " + query)
+    # result = client.query(query)
 
-    for measurement in result.get_points(measurement='stocks'):
-        usage_system = measurement['ticker']
-        open = measurement['open']
-        close = measurement['close']
-        print("{0}\t{1}\t{2}".format(usage_system, open, close))
+    # for measurement in result.get_points(measurement='ResponseTime'):
+    #     curtime = measurement['time']
+    #     timeval = measurement['rtime']
+    #     print("{0}\t{1}".format(curtime, timeval))
 
     # while(1):
     #     logger.info("NexGen Test starting..")
--- NexGenTest/NexGenTest/influxtest.py
+++ NexGenTest/NexGenTest/influxtest.py
@@ -1,28 +1,50 @@
 from influxdb import InfluxDBClient
 from datetime import datetime
+import yfinance as yf
+# from influxdb_client import InfluxDBClient, Point, WritePrecision
+# from influxdb_client.client.write_api import SYNCHRONOUS
 
-#Setup database
-client = InfluxDBClient('localhost', 8086, 'admin', 'Password1', 'mydb')
+token = "mySuP3rS3cr3tT0keN"
+# #Setup database
+client = InfluxDBClient('20.219.137.165', 8086, username=None, password=None, headers={"Authorization": token})
 client.create_database('mydb')
-client.get_list_database()
+print(client.get_list_database())
 client.switch_database('mydb')
 
 
-#Setup Payload
+# data = yf.download("MSFT", start='2022-07-01')
+# data = data.to_csv()
+
+# print(data)
+# #Setup Payload
 json_payload = []
-data = {
-    "measurement": "stocks",
-    "tags": {
-        "ticker": "TCS" 
-        },
-    "time": datetime.now(),
-    "fields": {
-        'open': 1688.37,
-        'close': 1667.93
+index = 0
+while(index <= 100):
+    data = {
+        "measurement": "stocks",
+        "tags": {
+            "ticker": "Infy" 
+            },
+        "time": datetime.now(),
+        "fields": {
+            'open': 1688.37,
+            'close': 1667.93
+        }
     }
-}
-json_payload.append(data)
+    json_payload.append(data)
+    index += 1
+
+    # #Send our payload
+    client.write_points(json_payload)
+
+# url = "http://20.219.137.165:8086"
+# org = "myorg"
+# bucket = "mybucket"
 
 
-#Send our payload
-client.write_points(json_payload)
+
+# client = InfluxDBClient(url=url, token=token, bucket=bucket)
+
+# write_api = client.write_api(SYNCHRONOUS)
+# write_api.write(bucket, org, data, write_precision='s')
+
